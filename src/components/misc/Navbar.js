77bf836fe29890580c659c3d4e4102e58495c8fc
Josue Valencia <josue.valencia@domo.ec>
2019-07-07 19:49:16 +0200

    new navbar

--- src/components/misc/Navbar.js
+++ src/components/misc/Navbar.js
@@ -1,39 +1,163 @@
 import React, {useState} from 'react'
-import {Link, NavLink} from 'react-router-dom'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import useStyles from './Navbar.style'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MailIcon from '@material-ui/icons/Mail';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuIcon from '@material-ui/icons/Menu';
+// import SearchIcon from '@material-ui/icons/Search';
+// import InputBase from '@material-ui/core/InputBase';
+import MoreIcon from '@material-ui/icons/MoreVert';
+// import {Link, NavLink} from 'react-router-dom'
+import AdapterLink from './LinkTalkit.js';
 
 const Navbar = () => {
-  const [isOpen, setDropdown] = useState(false);
-  
-  const menuClass = `collapse navbar-collapse ${isOpen ? " show" : ""}`;
+  // * Css
+  const classes = useStyles();
+  const menuId = 'primary-search-account-menu';
+  // * States
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);  
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  // * Hanlders 
+  const  handleProfileMenuOpen = (event) => setAnchorEl(event.currentTarget);
+  const  handleMobileMenuClose = () => setMobileMoreAnchorEl(null);
+  const  handleMenuClose = () => setAnchorEl(null);
+  const  handleMobileMenuOpen = (event) => setMobileMoreAnchorEl(event.currentTarget);
+  // * Menú element Web
+  const renderMenu = ( 
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}>
+      <MenuItem onClick={handleMenuClose} component={AdapterLink} to="/login">Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+  // * Menú element Mobil
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+    const renderMobileMenu = (
+      <Menu
+        anchorEl={mobileMoreAnchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={mobileMenuId}
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMobileMenuOpen}
+        onClose={handleMobileMenuClose}>
+        <MenuItem>
+          <IconButton aria-label="Show 4 new mails" color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <MailIcon />
+            </Badge>
+          </IconButton>
+          <p>Messages</p>
+        </MenuItem>
+        <MenuItem>
+          <IconButton aria-label="Show 11 new notifications" color="inherit">
+            <Badge badgeContent={11} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+          <p>Notifications</p>
+        </MenuItem>
+        <MenuItem onClick={handleProfileMenuOpen}>
+          <IconButton
+            aria-label="Account of current user"
+            aria-controls="primary-search-account-menu"
+            aria-haspopup="true"
+            color="inherit">
+            <AccountCircle />
+          </IconButton>
+          <p>Profile</p>
+        </MenuItem>
+      </Menu>
+    );
 
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">Home</Link>
-        <button 
-          onClick={() => setDropdown(!isOpen)}
-          className="navbar-toggler" 
-          type="button" 
-          data-toggle="collapse" 
-          data-target="#navbarNav" 
-          aria-controls="navbarNav" 
-          aria-expanded={isOpen} >
-          <span className="navbar-toggler-icon"></span>
-        </button>
 
-      <div className={menuClass} id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <NavLink activeClassName="active" className="nav-link" to="/search">Search</NavLink>
-          </li>          
-          <li className="nav-item">
-            <NavLink activeClassName="active" className="nav-link" to="/class">Private Route</NavLink>
-          </li>          
-          <li className="nav-item">
-            <NavLink activeClassName="active" className="nav-link" to="/logout">Logout</NavLink>
-          </li>          
-        </ul>
-      </div>
-    </nav>
-  )}
+            // {/* <NavLink activeClassName="active" className="nav-link" to="/search">Search</NavLink>
+            // <NavLink activeClassName="active" className="nav-link" to="/class">Private Route</NavLink>
+            // <NavLink activeClassName="active" className="nav-link" to="/logout">Logout</NavLink> */}
+return (
+    <div className={classes.grow}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            component={AdapterLink} to="/"
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Open drawer">
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Material-UI
+          </Typography>
+          {/* // ! Search Bar. 
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'Search' }}
+            />
+          </div> */}
+          <div className={classes.grow} />
+          {/* //* Section Desktop */}
+          <div className={classes.sectionDesktop}>
+            <IconButton aria-label="Show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="Show 17 new notifications" color="inherit">
+              <Badge badgeContent={17} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="Account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"  >
+              <AccountCircle />
+            </IconButton>
+          </div>
+          {/* //* Section Mobile */}
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="Show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit" >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
+  );
+}
 
 export default Navbar
